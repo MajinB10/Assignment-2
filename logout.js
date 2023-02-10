b507893877f80c00459088dae3314de782312c0b
Bhavesh <bhaveshultron@gmail.com>
2023-02-10 12:44:16 +0800

    Update to styling in profile page

--- logout.js
+++ logout.js
@@ -2,7 +2,7 @@ $(document).ready(function () {
     if ((localStorage.getItem("username") != null) &&(localStorage.getItem("pwd") != null) ){
         let content = `<div id="user-details"><h1 id="userinfo-username">Username:${localStorage.getItem("username")}</h1>
             <h2 id="userinfo-userEmail">Email:${localStorage.getItem("useremail")}</h2>
-            <h2 id="userinfo-userPoints">Total Points:${localStorage.getItem("points")}</h2></div>   <div id="logoutpos"><button id="logout" onclick="">LogOut</button><div>`;
+            <h2 id="userinfo-userPoints">TotalPoints:${localStorage.getItem("points")}</h2></div>   <div id="logoutpos"><button id="logout" onclick="">LogOut</button><div>`;
 
         document.getElementById("userinfo").innerHTML += content
     }
